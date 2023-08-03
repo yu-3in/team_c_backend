c6c65efd0f0cc08804a0b39f0817ce5ffdd7cdf7
taikicoco <kawasakitaiki0308@gmail.com>
2023-08-03 17:50:49 +0900

    update:GetTicket

--- repository/ticket.go
+++ repository/ticket.go
@@ -1,17 +1,47 @@
 package repository
 
 import (
+	"fmt"
+	"server/handler/request"
 	"server/model"
 )
 
-func (r *Repository) GetTickets() ([]*model.Ticket, error) {
+// Sort
+// 'latest_creation' // 作成日の新しい順
+// 'oldest_creation' // 古い順
+// 'latest_update' // 更新の新しい順
+// 'oldest_update' // 更新の古い順
+
+//filter
+// genreID
+// userID
+
+func (r *Repository) GetTickets(req request.ReqGetTicket) ([]*model.Ticket, error) {
 	var tickets []*model.Ticket
-	result := r.db.Preload("User").Preload("Genre").Find(&tickets)
+
+	query := r.db.Preload("User").Preload("Genre")
+
+	if req.GenreID != 0 {
+		fmt.Println("genreID", req.GenreID)
+		query = query.Where("genre_id = ?", req.GenreID)
+	}
+	
+	if req.UserID != 0 {
+		query = query.Where("user_id = ?", req.UserID)
+	}
+
+	// if req.Sort != "" {
+	// 	query = query.Order(req.Sort) // ソート順を指定
+	// }
+
+	result := query.Find(&tickets)
+
 	if result.Error != nil {
 		return nil, result.Error
 	}
 	return tickets, nil
 }
+
 
 func (r *Repository) GetTicket(id int) (*model.Ticket, error) {
 	var ticket model.Ticket
@@ -31,9 +61,11 @@ func (r *Repository) CreateTicket(ticket *model.Ticket) (*model.Ticket, error) {
 }
 
 func (r *Repository) UpdateTicket(ticket *model.Ticket) (*model.Ticket, error) {
-	result := r.db.Save(ticket)
-	if result.Error != nil {
-		return nil, result.Error
+	sql := `UPDATE tickets SET title=?, status=?, due_date=?, start_at=?, end_at=?, description=?, user_id=?, genre_id=?, created_at=?, updated_at=? WHERE id=?`
+	resuelt := r.db.Exec(sql, ticket.Title, ticket.Status, ticket.DueDate, ticket.StartAt, ticket.EndAt, ticket.Description, ticket.UserID, ticket.GenreID, ticket.CreatedAt, ticket.UpdatedAt, ticket.ID)
+
+	if resuelt.Error != nil {
+		return nil, resuelt.Error
 	}
 	return ticket, nil
 }
