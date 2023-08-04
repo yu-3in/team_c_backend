eeee97bcb94f8543beb9bb09fb6527bdb9a54e29
yuuumiravy <hikoki.y@gmail.com>
2023-08-04 10:44:31 +0900

    raiseHandUser

--- model/ticket.go
+++ model/ticket.go
@@ -12,7 +12,7 @@ type Ticket struct {
 	StartAt          time.Time `json:"startAt"`
 	EndAt            time.Time `json:"endAt"`
 	Description      string    `json:"description"`
-	RaisedHandUserId int       `json:"raisedHandUserId"`
+	RaisedHandUserId int       `json:"raisedHandUserId" gorm:"default:null"`
 	UserID           int       `json:"userId"`
 	GenreID          int       `json:"genreId"`
 	CreatedAt        time.Time `json:"createdAt"`
